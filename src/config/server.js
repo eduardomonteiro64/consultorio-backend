9514378b12ee551804281a1c0b208b48c1a70787
eduardo.carm <eduardo.carmo@sbwebservicesext.net>
2021-06-14 20:57:35 -0300

    undo all

--- src/config/server.js
+++ src/config/server.js
@@ -1,13 +1,16 @@
+const port = process.env.PORT || 8080;
+
+const bodyParser = require("body-parser");
 const express = require("express");
 const server = express();
 const allowCors = require("./cors");
 
-server.use(express.urlencoded({ extended: true }));
-server.use(express.json());
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
 server.use(allowCors);
 
-server.listen(process.env.PORT || 3000, function () {
-  console.log(`BACKEND is running on port ${process.env.PORT}.`);
+server.listen(port, function () {
+  console.log(`BACKEND is running on port ${port}.`);
 });
 
 module.exports = server;
