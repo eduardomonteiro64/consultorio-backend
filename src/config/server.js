eb7365e9c92d73d3756465f7d5f3d82e45926680
eduardo.carm <eduardo.carmo@sbwebservicesext.net>
2021-06-14 20:53:49 -0300

    remove bodyparser

--- src/config/server.js
+++ src/config/server.js
@@ -1,10 +1,9 @@
-const bodyParser = require("body-parser");
 const express = require("express");
 const server = express();
 const allowCors = require("./cors");
 
-server.use(bodyParser.urlencoded({ extended: true }));
-server.use(bodyParser.json());
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
 server.use(allowCors);
 
 server.listen(process.env.PORT || 3000, function () {
